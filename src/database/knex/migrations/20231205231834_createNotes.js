5a7ef5d5ad4c65b674823f284f08e7e9823bf66c
cariello <igor_cariello@hotmail.com.br>
2024-11-27 22:12:25 -0300

    create api rocketmovies

---
+++ src/database/knex/migrations/20231205231834_createNotes.js
@@ -0,0 +1,17 @@
+const { text } = require('express')
+const knex = require ('knex')
+
+exports.up = knex => knex.schema.createTable("notes", table => {
+table.increments('id')
+table.text('title')
+table.text('description')
+table.float('rating').notNullable().unsigned().checkBetween([0, 5])
+table.integer('user_id').references('id').inTable('users')
+
+table.timestamp('created_at').default(knex.fn.now())
+table.timestamp('updated_at').default(knex.fn.now())
+
+
+})
+
+exports.down = knex => knex.schema.dropTable('notes')
---
+++ src/database/knex/migrations/20231205231849_createTags.js
@@ -0,0 +1,12 @@
+const { text } = require('express')
+const knex = require ('knex')
+
+exports.up = knex => knex.schema.createTable("tags", table => {
+table.increments('id')
+table.integer('note_id').references('id').inTable('notes').onDelete('CASCADE')
+table.integer('user_id').references('id').inTable('users')
+table.text('name')
+
+})
+
+exports.down = knex => knex.schema.dropTable('tags')
